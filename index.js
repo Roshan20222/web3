32903fa0165c48ca27fbf3d9bf39b784f2b4270f
Roshan20222 <pandeyroshan20222@gmail.com>
2025-06-03 16:09:30 +0545

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const crypto = require('crypto');
 function findHashWithPrefix(prefix) {
     let input = 0;
     while (true) {
-        let inputStr = "100xdevs" + input.toString();
+        let inputStr = `
+harkirat => Raman | Rs 100
+Ram => Ankit | Rs 10
+` + input.toString();
         let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
         if (hash.startsWith(prefix)) {
             return { input: inputStr, hash: hash };
