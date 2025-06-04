0ef4b7a7d80ade55746c1d1d94ef4d188e62e8f7
Roshan20222 <pandeyroshan20222@gmail.com>
2025-06-04 09:39:53 +0545

    java script

---
+++ basic/index.js
@@ -0,0 +1,6 @@
+// console.log(`hello`);
+// console.log(`i like pizza`);
+
+// window.alert(`this is an alert`);
+document.getElementById("myH1").textContent = "Hello World!";
+document.getElementById("myP").textContent = "I like pizza!";
